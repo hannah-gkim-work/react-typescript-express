0b52d9442471e753ca84630a4ea472ec38a73a68
Hannah Gomez Kim <hannah.gomezkim@slalom.com>
2022-01-27 22:44:14 -0600

    first

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,9 +1,13 @@
-import * as express from 'express';
+import * as express from "express";
 
 const router = express.Router();
 
-router.get('/api/hello', (req, res, next) => {
-    res.json('World');
+router.get("/api/hello", (req, res, next) => {
+  res.json("Hello World");
+});
+
+router.get("/api/working", (req, res, next) => {
+  res.json("It is working");
 });
 
 export default router;
